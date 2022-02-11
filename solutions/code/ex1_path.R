95b320ee226f844e143a9e8f456eade195bbc5cf
dascolari <dascolari@utexas.edu>
2022-02-11 16:41:27 -0600

    ex1

--- solutions/code/ex1_path.R
+++
@@ -1,7 +0,0 @@
-# set user's unique path to the ECO395M directory
-# new users must edit here
-path <- "/Users/davidscolari/Dropbox/grad_school/3_Spring22/datmin"
-
-# make path to sub-directories
-path_data <- file.path(path, "ECO395M", "data")
-path_code <- file.path(path, "solutions", "code")
